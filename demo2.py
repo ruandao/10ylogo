d6a9844227ff07cb4c6ad949612ca158c2cddff2
ruandao <ljy080829@gmail.com>
2018-06-20 20:38:02 +0800

    0620/将图片添加到空白图片中: no message

--- demo2.py
+++ demo2.py
@@ -3,12 +3,16 @@ from PIL import ImageFont
 from PIL import Image
 from PIL import ImageDraw
 
+iconpath = "/Users/weixin/gitClone/10ylogo/icon/earphone.png"
 savepath = "/Users/weixin/gitClone/10ylogo/img/"
 fontPath = "/Users/weixin/Desktop/Font/Fonts/simsun.ttc"
 
+iconImg = Image.open(iconpath, 'r')
+
 font = ImageFont.truetype(fontPath,25)
 img=Image.new("RGBA", (200,200),(255,255,255))
+img.paste(iconImg, (0,0))
 draw = ImageDraw.Draw(img)
-draw.text((100, 100),"This is a test",(0,0,0),font=font)
+draw.text((10, 10),"This is a test",(0,0,0),font=font)
 draw = ImageDraw.Draw(img)
 img.save(savepath + "demo2.png")
